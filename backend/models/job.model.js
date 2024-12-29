fc2839ab436d1861440cf7c4d7c0d2ccaa5100d8
Nishant Gupta <gupta.nishant08042004@gmail.com>
2024-12-29 17:57:10 +0530

    create jobs post and get api

--- backend/models/job.model.js
+++ backend/models/job.model.js
@@ -23,6 +23,10 @@ const jobSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    experience: {
+        type: Number,
+        required: true
+    },
     position: {
         type: Number,
         required: true
