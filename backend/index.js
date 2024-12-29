0cc71021a2fe66490bb1f92238ab0ce30cfc0ccb
Nishant Gupta <gupta.nishant08042004@gmail.com>
2024-12-29 15:35:10 +0530

    create company register and update

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
+import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
 dotenv.config({});
 
 const app = express();
@@ -19,6 +21,11 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
+
+// api
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/company", companyRoute);
+
 
 app.listen(PORT, ()=>{
     connectDB();
